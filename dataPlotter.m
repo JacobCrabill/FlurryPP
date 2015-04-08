147ea08be2300372daf10d871b8437757db1d50d
JacobCrabill <jcrabill@stanford.edu>
2015-04-08 12:16:12 -0700

    Adding GIF printout to dataPlotter.

--- dataPlotter.m
+++ dataPlotter.m
@@ -1,13 +1,15 @@
 close all; clear all;
 
-Iters = [0:10:500];
-datadir = 'bin_debug';
-N = 50; % # of points to use in each direction for surf plot
+datadir = 'debug'; % Location of data files
+caseName = 'simData'; % Name of files to be plotted
+Iters = [0:100:500];    % Iterations to be plotted
+N = 50;                % # of points to use in each direction for surf plot
+GIF = false;           % Save plots to animated GIF file?
 
 data = cell(length(Iters));
 for i=1:length(Iters)
     iter = Iters(i);
-    filename = sprintf('%s/simData_%09d.vtk',datadir,iter);
+    filename = sprintf('%s/%s_%09d.vtk',datadir,caseName,iter);
     data{i} = csvread(filename);
 end
 
@@ -27,4 +29,16 @@ for i=1:length(Iters)
     U = F(X,Y);
     surf(X,Y,U);
     pause(.1);
+    
+	% Create animated GIF
+    if GIF
+        frame = getframe(1);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
+        if i == 1;
+          imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+        else
+          imwrite(imind,cm,filename,'gif','WriteMode','append');
+        end
+    end
 end
